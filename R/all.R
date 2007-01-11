91490b13a9f3723eda2ef7990585658b91dff016
Brian Ripley <ripley@stats.ox.ac.uk>
2007-01-11 00:00:00 +0000

    version 2.22-20

--- R/all.R
+++ R/all.R
@@ -1086,8 +1086,8 @@ sstdiag <- function(x,drv=0,degree=1,kernel="normal",
 
 .onLoad <- function(lib, pkg)
 {
-   cat("KernSmooth 2.22 installed\n")
-   cat("Copyright M. P. Wand 1997\n")
+   packageStartupMessage("KernSmooth 2.22 installed")
+   packageStartupMessage("Copyright M. P. Wand 1997")
 }
 
 .onUnload <- function(libpath)
